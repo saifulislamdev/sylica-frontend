7a903a9a4ea5a28d08c344ec1eaea55b24420b4a
joybiswas704 <joy.biswas704@gmail.com>
2021-10-24 10:09:14 -0400

    fixed the merge conflict with App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,47 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { Container } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Checkout from "./Checkout/Checkout";
-import LogIn from "./LogIn/LogIn";
-import Navbar from "./Navbar/Navbar";
-import SignUp from "./SignUp/SignUp";
-import Specifications from "./Product/Specifications";
+import Checkout from './Checkout/Checkout';
+import LogIn from './LogIn/LogIn';
+import Navbar from './Navbar/Navbar';
+import SignUp from './SignUp/SignUp';
+import Specifications from './Product/Specifications';
 
-import "../styles/App.css";
+import '../styles/App.css';
 
 function App() {
-    return (
-        <ChakraProvider>
-            <Container maxWidth="container.xl" p={0}>
-                <Navbar />
-                <Router>
-                    <Switch>
-                        <Route path="/auth/signup" exact>
-                            <SignUp />
-                        </Route>
-                        <Route path="/auth/signin" exact>
-                            <LogIn />
-                        </Route>
-                        {/* TODO: remove later (temporary) */}
-                        <Route path="/product/product-details" exact>
-                            <Specifications
-                                specifications={[
-                                    { Text: [{ a: "b" }] },
-                                    { Text: [{ cd: "ef" }, { gh: "ij" }] },
-                                    { Text: [{ klm: "nop" }, { qrs: "tuv" }] },
-                                    {
-                                        Text: [
-                                            { w: "x" },
-                                            { y: "z" },
-                                            { z: "z" },
-                                        ],
-                                    },
-                                ]}
-                            />
-                        </Route>
-                        {/* temporary */}
-                    </Switch>
-                </Router>
-            </Container>
-        </ChakraProvider>
-    );
+	return (
+		<ChakraProvider>
+			<Container maxWidth='container.xl' p={0}>
+				<Navbar />
+				<Router>
+					<Switch>
+						<Route path='/auth/signup' exact>
+							<SignUp />
+						</Route>
+						<Route path='/auth/signin' exact>
+							<LogIn />
+						</Route>
+						{/* TODO: remove later (temporary) */}
+						<Route path='/product/product-details' exact>
+							<Specifications
+								specifications={[
+									{ Text: [{ a: 'b' }] },
+									{ Text: [{ cd: 'ef' }, { gh: 'ij' }] },
+									{ Text: [{ klm: 'nop' }, { qrs: 'tuv' }] },
+									{
+										Text: [{ w: 'x' }, { y: 'z' }, { z: 'z' }],
+									},
+								]}
+							/>
+						</Route>
+						{/* temporary */}
+					</Switch>
+				</Router>
+			</Container>
+		</ChakraProvider>
+	);
 }
 
 export default App;
