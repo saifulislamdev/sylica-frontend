ba8a198ee653df7b4d7be01713234d8b8c6b09d5
Saiful Islam <nysaifulislam@gmail.com>
2021-11-03 03:11:09 -0400

    created ProductDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,49 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { Container } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Checkout from "./Checkout/Checkout";
-import LogIn from "./LogIn/LogIn";
-import Navbar from "./Navbar/Navbar";
-import SignUp from "./SignUp/SignUp";
-import Specifications from "./Product/Specifications";
+import Checkout from './Checkout/Checkout';
+import LogIn from './LogIn/LogIn';
+import Navbar from './Navbar/Navbar';
+import SignUp from './SignUp/SignUp';
+import ProductDetail from './Product/ProductDetail';
+import Specifications from './Product/Specifications';
 
-import "../styles/App.css";
+import '../styles/App.css';
 
 function App() {
     return (
         <ChakraProvider>
-            <Container maxWidth="container.xl" p={0}>
+            <Container maxWidth='container.xl' p={0}>
                 <Navbar />
                 <Router>
                     <Switch>
-                        <Route path="/auth/signup" exact>
+                        <Route path='/auth/signup' exact>
                             <SignUp />
                         </Route>
-                        <Route path="/auth/signin" exact>
+                        <Route path='/auth/signin' exact>
                             <LogIn />
                         </Route>
                         {/* TODO: remove later (temporary) */}
-                        <Route path="/product/product-details" exact>
+                        <Route path='/product/product-details' exact>
+                            <ProductDetail
+                                productTitle='Product Title'
+                                productDescription='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+                                price='0.00'
+                                maxQuantity='20'
+                            />
+                        </Route>
+                        <Route path='/product/product-specifications' exact>
                             <Specifications
                                 specifications={[
-                                    { Text: [{ a: "b" }] },
-                                    { Text: [{ cd: "ef" }, { gh: "ij" }] },
-                                    { Text: [{ klm: "nop" }, { qrs: "tuv" }] },
+                                    { Text: [{ a: 'b' }] },
+                                    { Text: [{ cd: 'ef' }, { gh: 'ij' }] },
+                                    { Text: [{ klm: 'nop' }, { qrs: 'tuv' }] },
                                     {
                                         Text: [
-                                            { w: "x" },
-                                            { y: "z" },
-                                            { z: "z" },
+                                            { w: 'x' },
+                                            { y: 'z' },
+                                            { z: 'z' },
                                         ],
                                     },
                                 ]}
---
+++ src/components/Product/ProductDetail.js
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import {
+    Button,
+    Container,
+    Flex,
+    FormControl,
+    Heading,
+    Icon,
+    IconButton,
+    NumberInput,
+    NumberInputField,
+    Tag,
+    Text,
+} from '@chakra-ui/react';
+import { BsCartPlus } from 'react-icons/bs';
+import {
+    MdOutlineArrowBackIosNew,
+    MdOutlineArrowForwardIos,
+} from 'react-icons/md';
+
+import { colors } from '../../util/constants';
+
+const ProductDetail = ({
+    productTitle,
+    productDescription,
+    price,
+    maxQuantity,
+}) => {
+    const [quantity, setQuantity] = useState(0);
+
+    return (
+        <Container>
+            <Heading mb='16px' size='lg'>
+                {productTitle}
+            </Heading>
+            <Text mb='32px' size='xs'>
+                {productDescription}
+            </Text>
+            <Text color={colors.neutralGray} fontSize='xs'>
+                Price
+            </Text>
+            <Text fontSize='2xl' mb='16px'>{`$${price}`}</Text>
+            {parseInt(maxQuantity) === 0 && (
+                <Tag
+                    bg={colors.primary}
+                    color={colors.neutralWhite}
+                    colorScheme={colors.primary}
+                    mb='16px'
+                    px='16px'
+                    variant='subtle'
+                >
+                    Out of Stock
+                </Tag>
+            )}
+            <Text color={colors.neutralGray} fontSize='xs'>
+                Quantity
+            </Text>
+            <Flex my='10px'>
+                <IconButton
+                    onClick={() => {
+                        setQuantity(quantity - 1);
+                    }}
+                    isDisabled={quantity === 0}
+                    icon={<MdOutlineArrowBackIosNew />}
+                    bg={colors.secondary}
+                    opacity='0.5'
+                    order='0'
+                    size='md'
+                    aria-label='Decrease quantity by 1'
+                />
+                <FormControl
+                    id='quantity'
+                    isDisabled={parseInt(maxQuantity) === 0}
+                    isInvalid={isNaN(+quantity)}
+                    maxW={`${Math.floor(Math.log10(quantity + 1)) * 8 + 60}px`} // expands automatically by expanding 8px for every digit
+                    mx='8px'
+                    order='1'
+                    size='sm'
+                    variant='input'
+                >
+                    <NumberInput
+                        // onChange={handleChange}
+                        value={quantity}
+                        defaultValue={1 ? parseInt(maxQuantity) : 0}
+                        max={parseInt(maxQuantity)}
+                        min={0}
+                        allowMouseWheel='true'
+                        inputMode='numerical'
+                        opacity='0.5'
+                    >
+                        <NumberInputField />
+                    </NumberInput>
+                </FormControl>
+                <IconButton
+                    onClick={() => {
+                        setQuantity(quantity + 1);
+                    }}
+                    isDisabled={quantity === parseInt(maxQuantity)}
+                    icon={<MdOutlineArrowForwardIos />}
+                    bg={colors.secondary}
+                    opacity='0.5'
+                    order='2'
+                    size='md'
+                    aria-label='Increase quantity by 1'
+                />
+                <Button
+                    // onClick={} // TODO: implemented later
+                    isDisabled={quantity === 0}
+                    bg={colors.primary}
+                    colorScheme={colors.primary}
+                    flexDirection='row'
+                    ml='8px'
+                    order='3'
+                    w='full'
+                >
+                    <Text
+                        color={colors.neutralWhite}
+                        fontSize='sm'
+                        fontWeight='semibold'
+                        mx='8px'
+                    >
+                        Add to Cart
+                    </Text>
+                    <Icon as={BsCartPlus} color={colors.neutralWhite}></Icon>
+                </Button>
+            </Flex>
+            <Button
+                // onClick={} // TODO: implemented later
+                isDisabled={quantity === 0}
+                isFullWidth={true}
+                borderColor={colors.primary}
+                colorScheme={colors.primary}
+                size='sm'
+                variant='outline'
+            >
+                Buy now
+            </Button>
+        </Container>
+    );
+};
+
+export default ProductDetail;
