ed2c9d21b91d2423e69e6269a95dabc24229ef16
Saiful Islam <nysaifulislam@gmail.com>
2021-11-14 01:24:05 -0500

    created Product page for individual product pages

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,8 @@ import Checkout from './Checkout/Checkout';
 import LogIn from './LogIn/LogIn';
 import Navbar from './Navbar/Navbar';
 import SignUp from './SignUp/SignUp';
-import ProductDetail from './Product/ProductDetail';
-import ProductDetailImages from './Product/ProductDetailImages';
+import Product from '../pages/Product';
 import ProductListing from './Product/ProductListing';
-import Specifications from './Product/Specifications';
 
 import '../styles/App.css';
 
@@ -26,48 +24,13 @@ function App() {
                         <Route path='/auth/signin' exact>
                             <LogIn />
                         </Route>
+                        <Route path='/products/:id' exact>
+                            <Product />
+                        </Route>
                         <Route path='/checkout' exact>
                             <Checkout />
                         </Route>
                         {/* TODO: remove later (temporary) */}
-                        <Route path='/product/product-details' exact>
-                            <ProductDetail
-                                productTitle='Product Title'
-                                productDescription='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-                                price='0.00'
-                                maxQuantity='20'
-                            />
-                        </Route>
-                        <Route path='/product/product-specifications' exact>
-                            <Specifications
-                                specifications={[
-                                    {
-                                        heading: 'General',
-                                        rows: [
-                                            ['a', 'b'],
-                                            ['c', 'd'],
-                                            ['e', 'f'],
-                                        ],
-                                    },
-                                    {
-                                        heading: 'CPU',
-                                        rows: [
-                                            ['g', 'h'],
-                                            ['i', 'j'],
-                                            ['k', 'l'],
-                                        ],
-                                    },
-                                    {
-                                        heading: 'Screen',
-                                        rows: [
-                                            ['m', 'n'],
-                                            ['o', 'p'],
-                                            ['q', 'r'],
-                                        ],
-                                    },
-                                ]}
-                            />
-                        </Route>
                         <Route path='/product/product-listing' exact>
                             <ProductListing
                                 description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
@@ -76,36 +39,6 @@ function App() {
                                 quantity='20'
                                 title='Product Title'
                             />
-                        </Route>
-                        <Route path='/product/product-detail-images' exact>
-                            <ProductDetailImages
-                                images={[
-                                    {
-                                        name: 'Macbook Air',
-                                        src: 'https://helios-i.mashable.com/imagery/reviews/03y8gbj1mqCuexgXxFJ5vyX/hero-image.fill.size_1248x702.v1623391330.jpg',
-                                    },
-                                    {
-                                        name: 'Another Cool Macbook Air',
-                                        src: 'https://m.media-amazon.com/images/I/71vFKBpKakL._AC_SL1500_.jpg',
-                                    },
-                                    {
-                                        name: 'M1 Macbook Air',
-                                        src: 'https://images.macrumors.com/t/KQrinkLLQlIz7W4LsVDGL2JzK64=/1600x0/article-new/2013/09/m1-macbook-air-design.jpg',
-                                    },
-                                    {
-                                        name: 'Space Gray Macbook Air',
-                                        src: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-macbook-air-space-gray-202002_AV2?wid=1000&hei=1000&fmt=jpeg&qlt=95&.v=1592436917000',
-                                    },
-                                    {
-                                        name: 'Another M1 Macbook Air',
-                                        src: 'https://photos5.appleinsider.com/gallery/39133-74930-M1-MacBook-Air-Thumbnail-xl.jpg',
-                                    },
-                                    {
-                                        name: '2022 Macbook Air',
-                                        src: 'https://telecomtalk.info/wp-content/uploads/2021/10/macbook-air-2022-might-not-be-much.jpeg',
-                                    },
-                                ]}
-                            ></ProductDetailImages>
                         </Route>
                         {/* temporary */}
                     </Switch>
---
+++ src/pages/Product.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { SimpleGrid, Text } from '@chakra-ui/react';
+
+import ProductDetail from '../components/Product/ProductDetail';
+import ProductDetailImages from '../components/Product/ProductDetailImages';
+import Specifications from '../components/Product/Specifications';
+
+import { axiosInstance } from '../util/config';
+
+export default function Products() {
+    const [productInfo, setProductInfo] = useState();
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        axiosInstance
+            .get(`/products/${id}`)
+            .then((res) => {
+                const product = res.data.product;
+                setProductInfo({
+                    description: product.description,
+                    images: product.images,
+                    price: product.price,
+                    quantity: product.quantity,
+                    specifications: product.specifications,
+                    title: product.title,
+                });
+            })
+            .catch((err) => {
+                setErrorMessage(err.response.data.msg); // msg is the field for error message from backend
+                setError(true);
+            });
+    }, [id]);
+
+    return !error && productInfo ? (
+        <SimpleGrid spacingY='32px'>
+            <SimpleGrid row columns={2} minChildWidth='320px' spacingY='32px'>
+                <SimpleGrid column>
+                    {productInfo.images && productInfo.quantity && (
+                        <ProductDetailImages
+                            images={productInfo.images}
+                            maxQuantity={productInfo.quantity}
+                        ></ProductDetailImages>
+                    )}
+                </SimpleGrid>
+                <SimpleGrid column>
+                    {productInfo.title &&
+                        productInfo.description &&
+                        productInfo.price &&
+                        productInfo.quantity && (
+                            <ProductDetail
+                                productTitle={productInfo.title}
+                                productDescription={productInfo.description}
+                                price={productInfo.price}
+                                maxQuantity={productInfo.quantity}
+                            />
+                        )}
+                </SimpleGrid>
+            </SimpleGrid>
+            {productInfo.specifications && (
+                <Specifications specifications={productInfo.specifications} />
+            )}
+        </SimpleGrid>
+    ) : (
+        <Text color='red' pl='16px'>{errorMessage}</Text>
+    );
+}
