13d099a89938cbd4b45ff626ff4907b347663fd7
Joy Biswas <joy.biswas704@gmail.com>
2021-11-15 08:57:21 -0500

    removed maxQuantity from the cart and added two more functions regarding adding and removing item from cart

--- src/components/CartContextWrapper/CartContextWrapper.js
+++ src/components/CartContextWrapper/CartContextWrapper.js
@@ -8,7 +8,6 @@ export const CartContextWrapper = ({ children }) => {
       id: 1,
       unitPrice: 1200,
       quantity: 1,
-      maxQuantity: 5,
       imageURL:
         "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFjYm9vayUyMHByb3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
       title: "Macbook Pro",
@@ -18,17 +17,15 @@ export const CartContextWrapper = ({ children }) => {
       id: 2,
       unitPrice: 599.99,
       quantity: 1,
-      maxQuantity: 2,
       imageURL:
         "https://cdn.tmobile.com/content/dam/t-mobile/en-p/cell-phones/apple/Apple-iPhone-12-mini/PRODUCT-RED/Apple-iPhone-12-mini-PRODUCT-RED-frontimage.png",
       title: "Iphone 11",
       description: "Lorem ipsum dolor sit amet",
     },
     {
-      id: 3,
+      id: "sfsfsf",
       unitPrice: 699.99,
       quantity: 1,
-      maxQuantity: 5,
       imageURL:
         "https://90a1c75758623581b3f8-5c119c3de181c9857fcb2784776b17ef.ssl.cf2.rackcdn.com/630282_195107_01_front_thumbnail.jpg",
       title: "Ryzen 9 5950x",
@@ -38,7 +35,6 @@ export const CartContextWrapper = ({ children }) => {
       id: 4,
       unitPrice: 5,
       quantity: 1,
-      maxQuantity: 1,
       imageURL: "https://bit.ly/2Z4KKcF",
       title: "Product Title 4",
       description: "Lorem ipsum dolor sit amet",
@@ -49,7 +45,7 @@ export const CartContextWrapper = ({ children }) => {
     let totalItem = 0;
     try {
       cart.forEach((product) => {
-        totalItem = totalItem + product.quantity;
+        totalItem = totalItem + parseInt(product.quantity);
       });
       return totalItem;
     } catch (error) {
@@ -84,6 +80,32 @@ export const CartContextWrapper = ({ children }) => {
     }
   };
 
+  const handleAddToCart = ({
+    id,
+    quantity,
+    unitPrice,
+    imageURL,
+    title,
+    description,
+  }) => {
+    const newProduct = {
+      id,
+      unitPrice,
+      quantity,
+      imageURL,
+      title,
+      description,
+    };
+    const newCart = [...cart, newProduct];
+    console.log(newCart);
+    setCart(newCart);
+  };
+
+  const handleRemoveItemFromCart = (id) => {
+    const newCart = cart.filter((product) => product.id !== id);
+    setCart(newCart);
+  };
+
   return (
     <CartContext.Provider
       value={{
@@ -91,6 +113,8 @@ export const CartContextWrapper = ({ children }) => {
         setCart,
         calculateTotalItemsInCart,
         handleAddQuantity,
+        handleAddToCart,
+        handleRemoveItemFromCart,
         calculateTotalPriceInCart,
       }}
     >
