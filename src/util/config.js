0db143476479e949bbbb29541fe60cfc27ca0e6f
Joy Biswas <joy.biswas704@gmail.com>
2021-11-15 17:52:41 -0500

    updated files to avoid merge conflict

--- src/util/config.js
+++ src/util/config.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+export const API_BASE_URL =
+    process.env.REACT_APP_PROXY || 'http://localhost:5000/api';
+
 export const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_PROXY || 'http://localhost:5000/api',
+    baseURL: API_BASE_URL,
 });
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,7 +1,8 @@
 export const colors = {
-	primary: 'teal.500',
-	secondary: 'teal.50',
-	neutralGray: 'gray.500',
-	neutralLighterGray: 'gray.200',
-	neutralWhite: 'white',
+    colorScheme: 'teal',
+    primary: 'teal.500',
+    secondary: 'teal.50',
+    neutralGray: 'gray.500',
+    neutralLighterGray: 'gray.200',
+    neutralWhite: 'white',
 };
