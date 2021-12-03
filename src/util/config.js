e88181e87e0c2ee02e369f6427caae211676f0af
Joy Biswas <joy.biswas704@gmail.com>
2021-12-03 14:00:57 -0500

    fixed the REACT_APP_PROXY to 5000 port

--- src/util/config.js
+++ src/util/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API_BASE_URL =
-  process.env.REACT_APP_PROXY || 'http://localhost:4242/api';
+  process.env.REACT_APP_PROXY || 'http://localhost:5000/api';
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
