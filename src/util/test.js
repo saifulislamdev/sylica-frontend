a1990116142cc555710fa876808652fb2f98a8f8
Saiful Islam <nysaifulislam@gmail.com>
2021-12-06 02:35:03 -0500

    added custom render with router for testing - added in another PR

--- src/util/test.js
+++ src/util/test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 const AllTheProviders = ({ children }) => {
@@ -18,3 +19,8 @@ export * from '@testing-library/react';
 
 // override render method
 export { customRender as render };
+
+export const renderWithRouter = (ui, { route = '/' } = {}) => {
+    window.history.pushState({}, 'Test page', route);
+    return render(ui, { wrapper: Router });
+};
